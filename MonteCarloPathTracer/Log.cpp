5f03c5eedfe5e669e4985c5b682aba431ad248be
huchengbei <huqiao@imhuqiao.com>
2019-03-20 14:59:16 +0800

    optimezed log, write to file

--- MonteCarloPathTracer/Log.cpp
+++ MonteCarloPathTracer/Log.cpp
@@ -1,25 +1,54 @@
 #include "iostream"
 #include "string"
 #include "chrono"
-#include "iomanip"
+#include "fstream"
 using namespace std;
 
 class Log 
 {
 public:
+	string path;
+	bool is2file = false;
+	ofstream outFile;
+	
 	Log() {}
+
+	void setPath(string p)
+	{
+		is2file = true;
+		path = p;
+		outFile.open(path);
+		if (!outFile.is_open())
+		{
+			outFile.close();
+			is2file = false;
+			out("open log file error!");
+		}
+		else
+		{
+			outFile.close();
+			out("open log file success!");
+		}
+	}
 
 	void out(string content)
 	{
 		using namespace chrono;
 		auto t = system_clock::to_time_t(system_clock::now());
 		struct tm* nowTime = localtime(&t);
-		cout << "[" << nowTime->tm_year + 1900 << setfill('0') << setw(2) << nowTime->tm_mon + 1
-			<< setfill('0') << setw(2) <<  (int)nowTime->tm_mday << " "
-			<< setfill('0') << setw(2) << (int)nowTime->tm_hour  << ":"
-			<< setfill('0') << setw(2) << (int)nowTime->tm_min << ":"
-			<< setfill('0') << setw(2) << (int)nowTime->tm_sec << "] "
-			<< content << endl;
+		char timeFormat[25];
+		sprintf_s(timeFormat, "[%d%02d%02d %02d:%02d:%02d] ", 
+			nowTime->tm_year + 1900, (int)nowTime->tm_mon + 1, 
+			(int)nowTime->tm_mday, (int)nowTime->tm_hour, 
+			(int)nowTime->tm_min, (int)nowTime->tm_sec);
+		string outContent = string(timeFormat) + content + "\n";
+		cout << outContent;
+		if (is2file)
+		{
+			outFile.open(path, fstream::out | ofstream::app);
+			outFile << outContent;
+			outFile.close();
+		}
 	}
 };
 
