0d89a5741e6da4604cefd3163865285ee008015d
huchengbei <huqiao@imhuqiao.com>
2019-03-24 08:32:36 +0800

    modify model

--- MonteCarloPathTracer/Main.cpp
+++ MonteCarloPathTracer/Main.cpp
@@ -32,6 +32,63 @@ Model*  model;
 void loadImage();
 void render(cv::Mat &image);
 
+// room model
+void loadSceneRoom()
+{
+	windowName = "Room - Monte Carlo Path Tracer";
+	cv::namedWindow(windowName);
+	saveImage = true;
+	savePreImage = 1;
+	log2file = true;
+	MaxRenderCnt = 100;
+
+	path = "../models/Scene01/room.obj";
+	int pos = (int)(path.find_last_of('.'));
+	resultDir = path.substr(0, pos) + "_results/";
+
+	if (log2file)
+		logs.setPath(path.substr(0, pos) + ".log");
+
+	pathTracer = PathTracer();
+	pathTracer.pxSampleNum = 1;
+	pathTracer.lightSampleNum = 3;
+	pathTracer.maxPathDepth = 5;
+	pathTracer.maxRenderDepth = MaxRenderCnt;
+
+	width = 200;
+	height = 200;
+	image = cv::Mat(height, width, CV_8UC3);
+	logs.out("Load Model...");
+	bool enableInternalLight = false;
+	model = new Model(path, enableInternalLight);
+	logs.out("Faces: " + to_string(model->facesNum) + "  Triangles: " + to_string(model->trianglesNum));
+	logs.out("Load Model...finished");
+	model->width = width;
+	model->height = height;
+	model->ambient = Color3f(0.4f, 0.4f, 0.4f);
+
+	vector<Light>* lights = model->lights;
+	Point3f lightCenter = Point3f(0.0, 1.589, -1.274);
+	float radius = 0.2f;
+	Color3f emission = Color3f(50, 50, 40);
+	// add external light
+	lights->push_back(Light(Light::TYPE::SPHERE, lightCenter, radius, emission * 2.0f));
+
+	Point3f position = Point3f(0.0, 0.0, 4);
+	Point3f center = Color3f(0.0, 0.0, 0.0);
+	Vec3f up = Vec3f(0, 1, 0);
+	fov = 50;
+
+	model->camera = new Camera(position, center, up);
+	Camera* &camera = model->camera;
+	camera->setViewPort(fov, (float)height / (float)width);
+
+	logs.out("Init Model And buildTree...");
+	model->init();
+	logs.out("Init Model And buildTree...finished");
+	loadImage();
+}
+
 // cup model
 void loadSceneCup()
 {
@@ -42,7 +99,7 @@ void loadSceneCup()
 	log2file = true;
 	MaxRenderCnt = 100;
 
-	path = "../models/Scene01/cup.obj";
+	path = "../models/Scene02/cup.obj";
 	int pos = (int)(path.find_last_of('.'));
 	resultDir = path.substr(0, pos) + "_results/";
 
@@ -80,63 +137,6 @@ void loadSceneCup()
 	Point3f center = Color3f(0.0, 0.40, 0.3);
 	Vec3f up = Vec3f(0, 1, 0);
 	fov = 60;
-
-	model->camera = new Camera(position, center, up);
-	Camera* &camera = model->camera;
-	camera->setViewPort(fov, (float)height / (float)width);
-
-	logs.out("Init Model And buildTree...");
-	model->init();
-	logs.out("Init Model And buildTree...finished");
-	loadImage();
-}
-
-// room model
-void loadSceneRoom()
-{
-	windowName = "Room - Monte Carlo Path Tracer";
-	cv::namedWindow(windowName);
-	saveImage = true;
-	savePreImage = 1;
-	log2file = true;
-	MaxRenderCnt = 100;
-
-	path = "../models/Scene02/room.obj";
-	int pos = (int)(path.find_last_of('.'));
-	resultDir = path.substr(0, pos) + "_results/";
-
-	if (log2file)
-		logs.setPath(path.substr(0, pos) + ".log");
-
-	pathTracer = PathTracer();
-	pathTracer.pxSampleNum = 1;
-	pathTracer.lightSampleNum = 3;
-	pathTracer.maxPathDepth = 5;
-	pathTracer.maxRenderDepth = MaxRenderCnt;
-
-	width = 512;
-	height = 512;
-	image = cv::Mat(height, width, CV_8UC3);
-	logs.out("Load Model...");
-	bool enableInternalLight = false;
-	model = new Model(path, enableInternalLight);
-	logs.out("Faces: " + to_string(model->facesNum) + "  Triangles: " + to_string(model->trianglesNum));
-	logs.out("Load Model...finished");
-	model->width = width;
-	model->height = height;
-	model->ambient = Color3f(0.2f, 0.2f, 0.2f);
-
-	vector<Light>* lights = model->lights;
-	Point3f lightCenter = Point3f(0.0, 1.589, -1.274);
-	float radius = 0.2f;
-	Color3f emission = Color3f(50, 50, 40);
-	// add external light
-	lights->push_back(Light(Light::TYPE::SPHERE, lightCenter, radius, emission * 2.0f));
-
-	Point3f position = Point3f(0.0, 0.0, 0.4);
-	Point3f center = Color3f(0.0, 0.0, 0.0);
-	Vec3f up = Vec3f(0, 1, 0);
-	fov = 50;
 
 	model->camera = new Camera(position, center, up);
 	Camera* &camera = model->camera;
@@ -288,10 +288,10 @@ void loadScene0()
 
 int main(int argc, char *argv[])
 {
+	loadSceneRoom();
 	// loadSceneCup();
-	// loadSceneRoom();
 	// loadSceneVeach();
-	loadScene0();
+	// loadScene0();
 	system("pause");
 	return 0;
 }
